e68a14be37e091c836f006478beb4c6f62e215fc
Mike West <mike.wesley.west@gmail.com>
2017-05-30 11:24:56 -0700

    Added more stations to the aka list

--- radiostations/radiostations/aka_call_sign.py
+++ radiostations/radiostations/aka_call_sign.py
@@ -4,8 +4,8 @@ Created on Jan 31, 2017
 @author: mwest
 
 Some radio stations display, or are known by, call signs other than the ones assigned by the FCC. The dictionary and
-function below exchange official FCC call signs for the ones the stations is better know by. This can occur for several reason but
-often a station is an AM or FM repeater or translator for a parent station or it could be a branding issue by the owners.
+function below exchange official FCC call signs for the ones the stations is better know by. This can occur for several reasons but
+often a station is an AM or FM repeater or translator for a parent station or it could be a branding decision by the owners.
 '''
 
 aka_call_signs = {
@@ -13,6 +13,18 @@ aka_call_signs = {
     , 'KZZQ': 'KZNS'
     , 'W237CW-FX': 'WDAE-FM'
     , 'KSPN': 'ESPNLA'
+    , 'K238BF': 'KPYN-FM'
+    , 'K265DV': 'KTON-FM'
+    , 'KIRO': 'ESPN Seattle'
+    , 'K271AH': 'KAPS-FM'
+    , 'WFAN': 'The FAN'
+    , 'WFAN-FM': 'The FAN FM'
+    , 'WIII': 'I-100'
+    , 'W262AD': 'I-100'
+    , 'WBGG': 'ESPN Pittsburgh'
+    , 'W287CP': 'WYTS-FM'
+    , 'WBIZ': 'The Brew'
+    , 'WBIZ-FM': 'Z-100'
     }
 
 def get_aka_sign(call_sign):
